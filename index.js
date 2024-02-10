04a752358d3f7f6f547029a71d63a1171fa3fac7
Fabio8520 <156619054+Fabio8520@users.noreply.github.com>
2024-02-10 17:37:46 -0300

    perguntas de arquitetura

--- index.js
+++ index.js
@@ -90,54 +90,55 @@ const perguntas = [
     correta: 1 // Resposta correta: "Jørn Utzon"
   }
 ];
-  
-  Seleciona a div com id quiz
-  const quiz = document.querySelector('#quiz')
-  //Seleciona a tag template
-  const template = document.querySelector('template');
-  
-  const corretas = new Set();
-  
-  const totalDePerguntas = perguntas.length;
-  const mostrarTotal = document.querySelector('#acertos span')
-  mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas;
-  
-  
-  // loop ou laço de repetição das perguntas
-  for(const item of perguntas){
-    //Clona todo o conteúdo do template e armazena em quizItem
-    const quizItem = template.content.cloneNode(true);
-    //Modifica o conteúdo de h3 adicionando as perguntas
-    quizItem.querySelector('h3').textContent = item.pergunta;
-  
-    // loop ou laço de repetição das respostas
-    for(const resposta of item.respostas) {
-      //Seleciona  e clona todo o conteúdo da dt
-      const dt = quizItem.querySelector('dl dt').cloneNode(true);
-      //Seleciona a tag span e atribui resposta a ela
-      dt.querySelector('span').textContent = resposta;
-      //Seleciona a tag input e adiciona os indices de perguntas
-      dt.querySelector('input').setAttribute('name', 'pergunta-', + perguntas.indexOf(item));
-      //Seleciona o valor de input e passa i indice da resposta
-      dt.querySelector('input').value = item.respostas.indexOf(resposta);
-      //verifica se o indice clicado está correto
-      dt.querySelector('input').onchange = (event) => {
-        const estaCorreta = event.target.value == item.correta;
-        
-        corretas.delete(item);
-        if(estaCorreta) {
-          corretas.add(item);
-        }
-        mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas;
-      };
-  
-      //Seleciona dl e adiciona o conteúdo de dt
-      quizItem.querySelector('dl').appendChild(dt);
-    }
-  
-      // remove o primeiro item de dt
-      quizItem.querySelector('dl dt').remove()
-  
-    // Coloca a pergunta na tela
-    quiz.appendChild(quizItem);
+
+
+//Seleciona a div com id quiz
+const quiz = document.querySelector('#quiz')
+//Seleciona a tag template
+const template = document.querySelector('template');
+
+const corretas = new Set();
+
+const totalDePerguntas = perguntas.length;
+const mostrarTotal = document.querySelector('#acertos span')
+mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas;
+
+
+// loop ou laço de repetição das perguntas
+for(const item of perguntas){
+  //Clona todo o conteúdo do template e armazena em quizItem
+  const quizItem = template.content.cloneNode(true);
+  //Modifica o conteúdo de h3 adicionando as perguntas
+  quizItem.querySelector('h3').textContent = item.pergunta;
+
+  // loop ou laço de repetição das respostas
+  for(const resposta of item.respostas) {
+    //Seleciona  e clona todo o conteúdo da dt
+    const dt = quizItem.querySelector('dl dt').cloneNode(true);
+    //Seleciona a tag span e atribui resposta a ela
+    dt.querySelector('span').textContent = resposta;
+    //Seleciona a tag input e adiciona os indices de perguntas
+    dt.querySelector('input').setAttribute('name', 'pergunta-', + perguntas.indexOf(item));
+    //Seleciona o valor de input e passa i indice da resposta
+    dt.querySelector('input').value = item.respostas.indexOf(resposta);
+    //verifica se o indice clicado está correto
+    dt.querySelector('input').onchange = (event) => {
+      const estaCorreta = event.target.value == item.correta;
+      
+      corretas.delete(item);
+      if(estaCorreta) {
+        corretas.add(item);
+      }
+      mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas;
+    };
+
+    //Seleciona dl e adiciona o conteúdo de dt
+    quizItem.querySelector('dl').appendChild(dt);
   }
+
+    // remove o primeiro item de dt
+    quizItem.querySelector('dl dt').remove()
+
+  // Coloca a pergunta na tela
+  quiz.appendChild(quizItem);
+}
